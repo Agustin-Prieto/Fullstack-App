a2625c423c4c7a5ef36eb788dba6e40ed450f3be
Agustin Prieto <agustinprieto.ar@gmail.com>
2019-02-27 01:04:01 -0300

    server configured

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalName));
+    }
+});
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Server
 app.listen(app.get('port'), () =>{
