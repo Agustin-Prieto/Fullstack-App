3abdb29439afbdb3a6d36a635c94a724ce7af60c
Agustin Prieto <agustinprieto.ar@gmail.com>
2019-03-20 15:38:25 -0300

    Rest API finished

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -4,8 +4,8 @@ const BookSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     isbn: { type: String, required: true },
-    imagePath: { type: String, required: true },
+    imagePath: { type: String },
     created_at: { type: Date, default: Date.now }
 });
 
-module.exports = model('book', bookSchema);
+module.exports = model('Book', BookSchema);
