1aa4903e1eb23068fd82e30d0de63b315b248f90
Agustin Prieto <agustinprieto.ar@gmail.com>
2019-03-21 17:48:10 -0300

    Static files added to index.js, webpack.config added script build added to package.json

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,9 @@ app.use(express.json());
 // Routes
 app.use('/api/books',require('./routes/books'));
 
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Server
 app.listen(app.get('port'), () =>{
     console.log('Server on port', app.get('port'));
